662c077cc7654732b9516eadd93d72218be3153c
Talut Salako <71020299+plutack1@users.noreply.github.com>
2023-08-03 18:19:44 +0000

    v1 aug3_2023

---
+++ downloader.py
@@ -0,0 +1,68 @@
+import os
+import base64
+import ffmpeg
+import requests
+from tqdm import tqdm
+
+
+def downloadVideo(masterJsonUrl, videoName, childPath):
+    masterJsonUrl= masterJsonUrl.replace("\u0026", "&")
+    
+    substrings = masterJsonUrl.split('/')
+    baseUrl = '/'.join(substrings[:5]) + '/parcel/'
+   
+    response = requests.get(masterJsonUrl)
+    content = response.json()
+
+    # Video download here
+    heights = [(i, d["height"]) for (i, d) in enumerate(content['video'])]
+    idx = max(heights, key=lambda x: x[1])
+    video = content['video'][idx[0]]
+    videoBaseUrl = baseUrl + video["base_url"]
+    filenameVideo = os.path.join(childPath, f'video_{video["id"]}')
+    
+    with open(filenameVideo, "wb") as file:
+        init_segment = base64.b64decode(video["init_segment"])
+        file.write(init_segment)
+        for segment in tqdm(video["segments"]):
+            segment_url = videoBaseUrl + segment["url"].replace("\u0026", "&")
+            response = requests.get(segment_url, stream=True)
+            if resp.status_code != 200:
+                print(response)
+                break
+            for chunk in response:
+                file.write(chunk)
+        file.flush()
+
+    # Audio download here
+    bitrate = [(i, d["bitrate"]) for (i, d) in enumerate(content['audio'])]
+    idx = max(bitrate, key=lambda x: x[1])
+    audio = content['audio'][idx[0]]
+    audioBaseUrl = baseUrl + audio["base_url"]
+    filenameAudio = os.path.join(childPath, f'video_{video["id"]}')
+
+    with open(filenameAudio, "wb") as file:
+        init_segment = base64.b64decode(audio["init_segment"])
+        file.write(init_segment)
+        for segment in tqdm(audio["segments"]):
+            segment_url = audioBaseUrl + segment["url"].replace("\u0026", "&")
+            response = requests.get(segment_url, stream=True)
+            if response.status_code != 200:
+                print(resp)
+                break
+            for chunk in response:
+                file.write(chunk)
+        file.flush()
+    
+    try:
+        inputVideo = ffmpeg.input(filenameVideo)
+        inputAudio = ffmpeg.input(filenameAudio)
+        ffmpeg.concat(inputVideo, inputAudio, v=1, a=1).output(os.path.join(childPath, videoName)).run()
+        print("Merge complete!\nVIDEO DOWNLOADED SUCCESSFULLY")
+    except ffmpeg.Error as error:
+        print(error.stderr)
+
+    print("Temporary files removed!")
+    os.remove(filenameVideo)
+    os.remove(filenameAudio)
+    
---
+++ main.py
@@ -0,0 +1,90 @@
+import os
+import requests
+from dotenv import load_dotenv
+from downloader import downloadVideo
+
+
+load_dotenv()
+
+LOGINURL = "https://actors-services.herokuapp.com/api/v1/auth/login"
+headers = {"Content-type": "application/json"}
+SECRET = {"email": os.getenv("EMAILADDRESS"), "password": os.getenv("PASSWORD")}
+
+session = requests.Session()
+session.post(LOGINURL, headers=headers, json=SECRET)
+
+
+CSRFTOKENURL = "https://portal.altschoolafrica.com/api/auth/csrf"
+response = session.get(CSRFTOKENURL))
+csrfTokenJson = response.json()
+csrfToken = csrfTokenJson["csrfToken"]
+
+
+
+CREDENTIALURL = "https://portal.altschoolafrica.com/api/auth/callback/credentials?"
+payload = {
+    "email": os.getenv("EMAILADDRESS"),
+    "password": os.getenv("PASSWORD"),
+    "redirect": "false",
+    "callbackUrl": "/applications/school",
+    "csrfToken": csrfToken,
+    "json": "true",
+}
+session.post(CREDENTIALURL, headers=headers, json=payload)
+
+
+
+sessionUrl = "https://portal.altschoolafrica.com/api/auth/session"
+response = session.get(sessionUrl)
+sessionInfo = response.json()
+bearerCode = sessionInfo["user"]["token"]
+
+headers = {"Authorization": f"Bearer {bearerCode}"}
+USERINFOURL = "https://actors-services.herokuapp.com/api/v1/auth/me" 
+response = session.get(USERINFOURL, headers=headers)
+userInfo = response.json()
+
+schoolId = userInfo["data"]["schoolId"]
+courseId = userInfo["data"]["courseId"]
+
+payload = {"limit": 50}
+STUDYKITURL = "https://actors-services.herokuapp.com/api/v1/study-kits"
+response = session.get(STUDYKITURL, headers=headers, params=payload)
+videoDetails = response.json()
+ 
+schoolCategory = videoDetails["data"]
+videoList = []
+
+for subcategory in schoolCategory:
+    if courseId == subcategory['facultyId']['_id']:
+        courseName = subcategory['facultyId']['name']
+
+    if schoolId == subcategory['schoolId']['_id'] and courseId == subcategory['facultyId']['_id']:
+        folderDict = {}
+        topic = subcategory['topic']
+        subtopics = subcategory['subTopics']    
+        folderDict['Topic'] = topic
+        folderDict['Subtopics'] = subtopics
+        videoList.append(folderDict)
+        
+rootPath = os.path.join(os.getcwd(), f'Altschool {courseName}')
+os.makedirs(rootPath)
+for folder in videoList:
+    childPath = os.path.join(rootPath, folder['Topic'])
+    os.makedirs(childPath)
+    for subtopic in folder['Subtopics']:
+        videoName = f"{subtopic['name']}.mp4"
+        videoUrl = subtopic['videoLink'].split('?')[0] + '/config'
+        response = requests.get(videoUrl)
+        data = response.json()
+        masterJsonUrl = data['request']['files']['dash']['cdns']['fastly_skyfire']['avc_url']
+        downloadVideo(masterJsonUrl, videoName, childPath)
+        print("study kit downloaded successfully!")
+
+
+
+        
+
+
+
+
