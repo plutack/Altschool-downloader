cf3532fcd06d4b58739cd39343928b56d47179ab
Talut Salako <71020299+plutack1@users.noreply.github.com>
2023-08-05 09:21:48 +0100

    file exist position correction

--- downloader.py
+++ downloader.py
@@ -14,57 +14,58 @@ def downloadVideo(masterJsonUrl, videoName, childPath):
     response = requests.get(masterJsonUrl)
     content = response.json()
 
+    if os.path.exists(os.path.join(childPath, videoName):
+        print(f"{videoName} exists"}
+    else:
     # Video download here
-    heights = [(i, d["height"]) for (i, d) in enumerate(content['video'])]
-    idx = max(heights, key=lambda x: x[1])
-    video = content['video'][idx[0]]
-    videoBaseUrl = baseUrl + video["base_url"]
-    filenameVideo = os.path.join(childPath, f'video_{video["id"]}')
-    
-    with open(filenameVideo, "wb") as file:
-        init_segment = base64.b64decode(video["init_segment"])
-        file.write(init_segment)
-        for segment in tqdm(video["segments"]):
-            segment_url = videoBaseUrl + segment["url"].replace("\u0026", "&")
-            response = requests.get(segment_url, stream=True)
-            if response.status_code != 200:
-                print(response)
-                break
-            for chunk in response:
-                file.write(chunk)
-        file.flush()
+        heights = [(i, d["height"]) for (i, d) in enumerate(content['video'])]
+        idx = max(heights, key=lambda x: x[1])
+        video = content['video'][idx[0]]
+        videoBaseUrl = baseUrl + video["base_url"]
+        filenameVideo = os.path.join(childPath, f'video_{video["id"]}')
+        
+        with open(filenameVideo, "wb") as file:
+            init_segment = base64.b64decode(video["init_segment"])
+            file.write(init_segment)
+            for segment in tqdm(video["segments"]):
+                segment_url = videoBaseUrl + segment["url"].replace("\u0026", "&")
+                response = requests.get(segment_url, stream=True)
+                if response.status_code != 200:
+                    print(response)
+                    break
+                for chunk in response:
+                    file.write(chunk)
+            file.flush()
 
-    # Audio download here
-    bitrate = [(i, d["bitrate"]) for (i, d) in enumerate(content['audio'])]
-    idx = max(bitrate, key=lambda x: x[1])
-    audio = content['audio'][idx[0]]
-    audioBaseUrl = baseUrl + audio["base_url"]
-    filenameAudio = os.path.join(childPath, f'audio_{audio["id"]}')
+        # Audio download here
+        bitrate = [(i, d["bitrate"]) for (i, d) in enumerate(content['audio'])]
+        idx = max(bitrate, key=lambda x: x[1])
+        audio = content['audio'][idx[0]]
+        audioBaseUrl = baseUrl + audio["base_url"]
+        filenameAudio = os.path.join(childPath, f'audio_{audio["id"]}')
 
-    with open(filenameAudio, "wb") as file:
-        init_segment = base64.b64decode(audio["init_segment"])
-        file.write(init_segment)
-        for segment in tqdm(audio["segments"]):
-            segment_url = audioBaseUrl + segment["url"].replace("\u0026", "&")
-            response = requests.get(segment_url, stream=True)
-            if response.status_code != 200:
-                print(resp)
-                break
-            for chunk in response:
-                file.write(chunk)
-        file.flush()
-    
-    try:
-        inputVideo = ffmpeg.input(filenameVideo)
-        inputAudio = ffmpeg.input(filenameAudio)
-        if os.path.exists(os.path.join(childPath, videoName):
+        with open(filenameAudio, "wb") as file:
+            init_segment = base64.b64decode(audio["init_segment"])
+            file.write(init_segment)
+            for segment in tqdm(audio["segments"]):
+                segment_url = audioBaseUrl + segment["url"].replace("\u0026", "&")
+                response = requests.get(segment_url, stream=True)
+                if response.status_code != 200:
+                    print(resp)
+                    break
+                for chunk in response:
+                    file.write(chunk)
+            file.flush()
+        
+        try:
+            inputVideo = ffmpeg.input(filenameVideo)
+            inputAudio = ffmpeg.input(filenameAudio) 
             ffmpeg.concat(inputVideo, inputAudio, v=1, a=1).output(os.path.join(childPath, videoName)).run() 
             print("Merge completed!\nVideo saved successfully")
-        else:
-            print("Video exists"} 
-    except ffmpeg.Error as error:
-        print(error.stderr)
 
-    os.remove(filenameVideo)
-    os.remove(filenameAudio)
-    print("Temporary files removed!")    
+        except ffmpeg.Error as error:
+            print(error.stderr)
+
+        os.remove(filenameVideo)
+        os.remove(filenameAudio)
+        print("Temporary files removed!")    
